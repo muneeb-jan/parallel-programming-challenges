26f43062aa9825eca35ecb646ba00eb17a155cb3
Muneeb H. Jan <muneeb_hafeez.jan@mailbox.tu-dresden.de>
2021-05-17 01:14:03 +0200

    Kmeans parallelized

--- mopp-2017-t4-k-means-clustering/kmeans.c
+++ mopp-2017-t4-k-means-clustering/kmeans.c
@@ -1,15 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <pthread.h>
+#include <sys/sysinfo.h>
+#include <assert.h>
+#include <stdint.h>
 #define DIM 3 //Dimensions
-int main(void) {
-	int i, j, k, n, c;
+
+int cpus, flips;
+int *cluster, *count;
+double *x, *mean, *sum;	
+int k, n;
+pthread_mutex_t lock;
+
+void *calculate (void *args)
+{
+
+	int deref = (intptr_t) args, color;
 	double dmin, dx;
-	double *x, *mean, *sum;
-	int *cluster, *count, color;
-	int flips;
+	double *sum_local = (double *)malloc(sizeof(double)*DIM*k);
+	int *count_local = (int *)malloc(sizeof(int)*k);
+	for (int j = 0; j < k; j++) {
+			count_local[j] = 0; //Initialize count to 0.
+			for (int i = 0; i < DIM; i++) 
+				sum_local[j*DIM+i] = 0.0; //Initialize sum to 0.
+		}
+
+	for (int i = deref; i < n; i=i+cpus) {
+			dmin = -1; color = cluster[i]; //Initialize color of point and min dist.
+			for (int c = 0; c < k; c++) { //assign the point to the nearest centroid.
+				dx = 0.0;
+				for (int j = 0; j < DIM; j++) 
+					dx +=  (x[i*DIM+j] - mean[c*DIM+j])*(x[i*DIM+j] - mean[c*DIM+j]); //(x-xk)^2 + (y-yk)^2 + (z-zk)^2
+				if (dx < dmin || dmin == -1) { //It will always run first time. 
+					color = c; // Point will get assigned to centroid with lowest dx 
+					dmin = dx;
+				}
+			}
+			if (cluster[i] != color) {
+				flips++; //Cluster of point changed. Needs to re-calculte mean.
+				cluster[i] = color;
+	      	}
+		}
+
+	for (int i = deref; i < n; i = i+cpus)
+	{
+		count_local[cluster[i]]++;
+		for (int j = 0; j < DIM; j++) 
+			sum_local[cluster[i]*DIM+j] += x[i*DIM+j];
+
+	}
+	pthread_mutex_lock(&lock);
+	for(int i = 0; i < k; i++)
+	{
+		count[i] += count_local[i]; 
+		for(int j=0; j < DIM; j++)
+			sum[i*DIM+j] += sum_local[i*DIM+j];
+	}
+	pthread_mutex_unlock(&lock);
+}
+
+int main(void) {
+	cpus = get_nprocs();
+	int i, j, c;
+
+ 	if (getenv("MAX_CPUS")) 
+        {
+                 cpus = atoi(getenv("MAX_CPUS"));
+        }
+    // Sanity-check
+    assert(cpus > 0 && cpus <= 64);
+    fprintf(stderr, "Running on %d CPUs\n", cpus);
+
+    pthread_t thread[cpus];         //declare total threads.
+	
 	scanf("%d", &k); // The number of centroids
 	scanf("%d", &n); // THe number of points
+
 	x = (double *)malloc(sizeof(double)*DIM*n); //Creating an array for n points with 3 dimensional values
 	mean = (double *)malloc(sizeof(double)*DIM*k); // Creating array for k centroids with 3D values to be calculated from point means.
 	sum= (double *)malloc(sizeof(double)*DIM*k);
@@ -30,30 +97,27 @@ int main(void) {
 				sum[j*DIM+i] = 0.0; //Initialize sum to 0.
 		}
 
-		//One parallelization here
-		for (i = 0; i < n; i++) {
-			dmin = -1; color = cluster[i]; //Initialize color of point and min dist.
-			for (c = 0; c < k; c++) { //assign the point to the nearest centroid.
-				dx = 0.0;
-				for (j = 0; j < DIM; j++) 
-					dx +=  (x[i*DIM+j] - mean[c*DIM+j])*(x[i*DIM+j] - mean[c*DIM+j]); //(x-xk)^2 + (y-yk)^2 + (z-zk)^2
-				if (dx < dmin || dmin == -1) { //It will always run first time. 
-					color = c; // Point will get assigned to centroid with lowest dx 
-					dmin = dx;
-				}
-			}
-			if (cluster[i] != color) {
-				flips++; //Cluster of point changed. Needs to re-calculte mean.
-				cluster[i] = color;
-	      	}
-		}
+		//printf("\nReached while loop\n");
+		pthread_mutex_init(&lock, NULL);
+		//One parallelization here. Divide the calculation of euclidean distance of points into n threads.
+ 		for (i = 0; i < cpus; i++)
+    	{
+            pthread_create(&thread[i], NULL, calculate, (void*)(intptr_t) i); // we cannot conver int to void* without warning due to size mismatch. So use intptr_t.
+    	}
+
+		// Thread join
+        for (i = 0; i < cpus; i++) 
+        {
+                pthread_join(thread[i], NULL);
+        }
+		pthread_mutex_destroy(&lock);
 
 		//One parallelization here. Have to create local count and sum array for each thead and then add finally. Lock section?
-	    for (i = 0; i < n; i++) {
+	    /*for (i = 0; i < n; i++) {
 			count[cluster[i]]++; //Count the points in each cluster
 			for (j = 0; j < DIM; j++) 
 				sum[cluster[i]*DIM+j] += x[i*DIM+j]; //Calculate the sum of the x,y,z coordinates of points in each cluster.
-		}
+		}*/
 		for (i = 0; i < k; i++) {
 			for (j = 0; j < DIM; j++) {
 				mean[i*DIM+j] = sum[i*DIM+j]/count[i]; //Calculate new mean.
